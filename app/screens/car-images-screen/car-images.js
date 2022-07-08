cae0fb84a9d7545fce6a4eac8238e22a9a4e8007
unknown <jintus740gmail.com>
2022-07-08 15:57:23 +0530

    add new screens with drawer

---
+++ app/screens/car-images-screen/car-images.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, View} from 'react-native';
+import Card from '../../components/card/image-card';
+import {getImages} from '../../services/api/get-images/get-car-images';
+
+// import {TouchableOpacity} from 'react-native-gesture-handler';
+
+export default function CarImages({navigation}) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const queryParameters = {
+      text: 'car',
+    };
+    const result = await getImages(queryParameters);
+    setData(result.photos?.photo);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const renderCard = ({item}) => {
+    let imgUrl = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
+
+    const showDetail = () => {
+      navigation.navigate('Detail', {cardData: item, imageUrl: imgUrl});
+    };
+
+    return (
+      <Card onPress={() => showDetail(item)} url={imgUrl} title={item.title} />
+    );
+  };
+
+  return (
+    <View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          numColumns={6 / 2}
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={renderCard}
+
+          // onEndReachedThreshold={0.5}
+        />
+      )}
+    </View>
+  );
+}
